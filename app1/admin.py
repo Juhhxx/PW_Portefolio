963524b7b46b7668955035ec79975de09d955a85
Julia Costa <juliacosta232005@gmail.com>
2026-04-11 15:17:57 +0100

    Add Course Model, Add some Entries via Admin App

--- app1/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from app1.models import Pessoa
-
-# Register your models here.
-admin.site.register(Pessoa)
--- portefolio/models.py
+++ portefolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    requirements = models.TextField()
+    flyer = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
