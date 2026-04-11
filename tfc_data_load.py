bb3f9e8c2b4898efafcf95af343e7f854c2d6ba5
Julia Costa <juliacosta232005@gmail.com>
2026-04-11 23:15:58 +0100

    Add JSON Reader Method and Test if it Works

--- tfc_data_load.py
+++ tfc_data_load.py
@@ -6,9 +6,16 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 django.setup()
 
+def get_tfc_data() -> list:
+    tfc_data = os.path.join(os.getcwd(), 'data', 'dados_tfcs.json')
+    
+    with open(tfc_data, 'r', encoding="utf8") as f:
+        data = json.load(f)
+        return data
+
 def run():
-    from portefolio.models import Course
-    print(Course.objects.count())
+    tfcs = get_tfc_data()
+    print(f"Total TFCs: {len(tfcs)}")
 
 if __name__ == "__main__":
     run()
