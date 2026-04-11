1105c60ad8bba41af97734ff11b93660588279d0
Julia Costa <juliacosta232005@gmail.com>
2026-04-11 23:14:52 +0100

    Test if tfc_data_load Script is Connecting Properly

--- tfc_data_load.py
+++ tfc_data_load.py
@@ -5,3 +5,10 @@ import json
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 django.setup()
+
+def run():
+    from portefolio.models import Course
+    print(Course.objects.count())
+
+if __name__ == "__main__":
+    run()
